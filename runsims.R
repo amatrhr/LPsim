a23a6c6b57888b6a0c53d606bd2edb9e00082e22
Charles Andre Laurin <cl14022@outlook.com>
2016-01-26 05:14:36 -0500

    dealing with functional structure; added dependency on plyr

--- runsims.R
+++ runsims.R
@@ -11,7 +11,7 @@
 
 
 # Other things that are required (loop over lambda and collect statistics on it)
-
+require(plyr) 
 require(glmnet)
 require(boot)
 require(cvTools)
@@ -63,12 +63,15 @@ data_gen <- function(N, psig, pnoise, model, rs , nsnps = 10000, hsq = 0.10){
         errors <- rnorm(N, mean = 0, sd = sqrt(1 - hsq) )
         lc <- scale(fullX)%*%b
         Y<- lc + errors
-        siglist <- b[1:floor(nsnps/1000)] 
-        nlist <- b[(floor(nsnps/1000)+1) : nsnps]
+        psig <- floor(nsnps/1000)
+        pnoise <- nsnps - psig
+        totalP <- nsnps
+        siglist <- b[1:psig]
+        nlist <- b[(psig+1) : nsnps]
         achR2 <- var(lc)/var(Y) 
     } 
 
-    output <- list(Y =Y, fullX = fullX,  achR2 = achR2, siglist = siglist, nlist = nlist)
+    output <- list(Y =Y, fullX = fullX,  achR2 = achR2, siglist = siglist, nlist = nlist, p = totalP, psig = psig)
 
     return(output) 
 
@@ -99,22 +102,23 @@ boot_rep_las <- function(data, idx, nested = FALSE, givenL = 0.01 ){
 
 }
 
-select_res <- function(siglist, nlist, coefs, method = "simple"){
+select_res <- function(sim_example, model_results){
 ### select_res() -> turns coefficients stats into selection stats
                                         # coefs is the vector of coefficients in the simple case but the matrix of bootstrap results in the fixboot and nested cases
     # lambda should be listed as one of the elements in the matrix of nested bootstrap results 
     
-    basevec <- rep(0, (length(siglist) + length(nlist)))
+    basevec <- rep(0, (length(sim_example$siglist) + length(sim_example$nlist)))
     
        # convert lists of indices to logical vectors (probably a clumsy way to do this)
     sigvec <- basevec
-    sigvec[siglist] <- 1 
+    sigvec[sim_example$siglist] <- 1 
         
     nvec <- basevec
-    nvec[nlist] <- 1 
+    nvec[sim_example$nlist] <- 1 
 
-    if(method == "simple"){
+    if(sim_example$method == "simple"){
     # If given a single result, produce: FPR, TPR, FNR, VSP
+        coefs <- model_results[-nrow(model_results), ] # lambda is always to be the bottom row!
         selects <- as.numeric(coefs != 0) # or abs() > 1e-9    
         TPs <- sum(selects*sigvec)
         TPR <- TPs/sum(sigvec + 1e-16)
@@ -124,11 +128,11 @@ select_res <- function(siglist, nlist, coefs, method = "simple"){
     
         VSP <- TPs /(TPs + FPs + 1e-16)
 
-        output <- list(VSP = VSP,  TPR = TPR, FPR = FPR, TNR = TNR)
+        output <- data.frame(list(VSP = VSP,  TPR = TPR, FPR = FPR, TNR = TNR))
    
-    } else if (method == "boot"){
+    } else if (sim_example$method == "boot"){
 
-        qselects <- as.numeric(coefs$qciL*coefs$qciU > 0) # or abs() > 1e-9
+        qselects <- as.numeric(model_results$qciL*model_results$qciU > 0) # or abs() > 1e-9
 
         qTPs <- sum(qselects*sigvec)
         qTPR <- qTPs/sum(sigvec + 1e-16)
@@ -140,7 +144,7 @@ select_res <- function(siglist, nlist, coefs, method = "simple"){
         qVSP <- qTPs /(qTPs + qFPs + 1e-16)
 #        qciLen <- coefs$qciU - coefs$qciL
 
-        ntselects <- as.numeric(coefs$ntciL*coefs$ntciU > 0) # or abs() > 1e-9
+        ntselects <- as.numeric(model_results$ntciL*model_results$ntciU > 0) # or abs() > 1e-9
 
         ntTPs <- sum(ntselects*sigvec)
         ntTPR <- ntTPs/sum(sigvec + 1e-16)
@@ -153,10 +157,11 @@ select_res <- function(siglist, nlist, coefs, method = "simple"){
  #       ntciLen <- coefs$ntciU - coefs$ntciL
 
         
-        output <- list(qVSP = qVSP,  qTPR = qTPR, qFPR = qFPR, qTNR = qTNR, ntVSP = ntVSP,  ntTPR = ntTPR, ntFPR = ntFPR, ntTNR = ntTNR)#, ntciLen = ntciLen , qciLen = qciLen)
+        output <- data.frame(list(qVSP = qVSP,  qTPR = qTPR, qFPR = qFPR, qTNR = qTNR, ntVSP = ntVSP,  ntTPR = ntTPR, ntFPR = ntFPR, ntTNR = ntTNR))#, ntciLen = ntciLen , qciLen = qciLen)
         
     # If given fixed-lambda bootstrap, produce: R/P & lengths for each kind of CI
-    } else if (method == "nested"){
+    } else if (sim_example$method == "nested"){
+        coefs <- model_results[-nrow(model_results), ] # lambda is always to be the bottom row!
         qselects <- as.numeric(coefs$qciL*coefs$qciU > 0) # or abs() > 1e-9
 
         qTPs <- sum(qselects*sigvec)
@@ -182,7 +187,7 @@ select_res <- function(siglist, nlist, coefs, method = "simple"){
  #       ntciLen <- coefs$ntciU - coefs$ntciL
   #      fivenumL <- fivenum(lambda)
         
-        output <- list(qVSP = qVSP,  qTPR = qTPR, qFPR = qFPR, qTNR = qTNR, ntVSP = ntVSP,  ntTPR = ntTPR, ntFPR = ntFPR, ntTNR = ntTNR)#,, qciLen = qciLen ntciLen = ntciLen, mL = mL, sdL = sdL, fivenumL = fivenumL)
+        output <- data.frame(list(qVSP = qVSP,  qTPR = qTPR, qFPR = qFPR, qTNR = qTNR, ntVSP = ntVSP,  ntTPR = ntTPR, ntFPR = ntFPR, ntTNR = ntTNR))#,, qciLen = qciLen ntciLen = ntciLen, mL = mL, sdL = sdL, fivenumL = fivenumL)
         
      # If given random-lambda bootstrap, produce: "" plus m/sd/5num lambda stats 
     }
@@ -191,13 +196,40 @@ select_res <- function(siglist, nlist, coefs, method = "simple"){
     return(output)
 }
 
-est_res <- function(){
+est_res <- function(sim_example, model_results){
 ### est_res() -> unscramble coefficient stats
+### and save to a data structure of some kind
+    results <- rbind(model_results[sim_example$siglist,], model_results[sim_example$nlist])
+    return(results) # should do in batches of e.g., 100, which will be handled by the replication function
 }
 
-replications <- function(){}
-=
-sim_init <- function(N = 2500, p = 100, r=0.5, model = 'Fix',nsnps = 10000, hsq = 0.10 ,seed = 4237, method = "simple",  rep = 0){
+replications <- function(conditions, batch_size){
+    # Handle replications over a batch of ~ 100
+    # Represent the size of an object
+    sel_obj <- paste("sel", 1:batch_size, sep = "")
+    est_obj <- paste("est", 1:batch_size, sep = "")
+
+    one_replication <- function(conditions, select_name = sel_name, estimates_name = est_name){
+
+        rep_init <- sim_init(N = conditions$N, p = conditions$p, r = conditions$r, model = conditions$model, nsnps = conditions$nsnps, hsq = conditions$hsq, seed = floor(1e7* runif(1)), method = conditions$method)
+        rep_boot <- boot_run(sim_example = rep_init)
+        rep_select <- select_res(sim_example = rep_init, model_results = rep_boot)
+        rep_est <- est_res(sim_example = rep_init, model_results = rep_boot)
+        assign(x = select_name, value = rep_select)
+        assign(x = estimates_name, value = rep_est)
+
+    }
+
+    for (i in 1:batch_size){
+
+        one_replication(conditions = conditions, select_name = sel_obj[i], estimates_name = est_obj[i])
+
+    }
+    
+    return(list(estimates = rbind.fill(lapply(sel_obj, get)), selects = rbind.fill(lapply(est_obj, get))))
+}
+
+sim_init <- function(N = 2500, p = 100, r=0.5, model = 'Fix',nsnps = 10000, hsq = 0.10 ,seed = 4237, method = "simple"){
 ### SPLIT THIS INTO A 'INITIAL MODEL' FUNCTION and a BOOTSTRAPPING FUNCTION
 #                                        # problem is that 'nested' keywoed doesn't need to be reused
     pnoise <- p - 1
@@ -212,13 +244,13 @@ sim_init <- function(N = 2500, p = 100, r=0.5, model = 'Fix',nsnps = 10000, hsq 
     modelData <- data_gen(N = N, psig = 1, pnoise = pnoise, model = model, rs = r, nsnps = nsnps, hsq = hsq)
 
     lasso <- cv.glmnet(x = modelData$fullX, y = modelData$Y)
-    return(list(lasso = lasso, modelData = modelData, N = N, model = model, rs = r, nsnps = nsnps, hsq = hsq, nsnps = nsnps, p = p, rep = rep, method = method))
+    return(list(lasso = lasso, modelData = modelData, N = N, model = model, rs = r, nsnps = nsnps, hsq = hsq, nsnps = nsnps, p = p,  method = method))
 }
 
 boot_run <- function(sim_example){
     if(sim_example$method == "simple"){
 
-        output <- matrix(c(coef(sim_example$lasso, s = sim_example$lasso$lambda.min)[-1], sim_example$lasso$lambda.min))
+        output <- data.frame(matrix(c(coef(sim_example$lasso, s = sim_example$lasso$lambda.min)[-1], sim_example$lasso$lambda.min)))
         
     } else if (sim_example$method == "boot"){
      # any kind of bootstrapping, not just nested, so will need to add logic to handle such a case
@@ -240,7 +272,7 @@ boot_run <- function(sim_example){
             blasmat[j, 13] <- cires$normal[,3] - cires$normal[,2]
         }
 
-        output <- blasmat
+        output <- data.frame(blasmat)
 
     } else if (sim_example$method == "nested"){
         nested <- TRUE
@@ -261,7 +293,7 @@ boot_run <- function(sim_example){
             blasmat[j, 13] <- cires$normal[,3] - cires$normal[,2]
         }
                 
-        output <- blasmat
+        output <- data.frame(blasmat)
         
     }
 
@@ -269,13 +301,13 @@ boot_run <- function(sim_example){
 
 }
 
-multi_rep_output <- function(output_list, method = "simple"){
-    # Aggregate summary stats over all replications? How to do this?
-    # It's wasteful to save to text file but the replicate function isn't going to handle a list with a big matrix element
-    # Selection stats
+## multi_rep_output <- function(output_list, method = "simple"){
+##     # Aggregate summary stats over all replications? How to do this?
+##     # It's wasteful to save to text file but the replicate function isn't going to handle a list with a big matrix element
+##     # Selection stats
 
-    # Estimate summary stats
+##     # Estimate summary stats
 
-    # Yeah, the above code is not right! Need to return a list, p + 1 coefficeients, or similarly a p+1 matrix of bootstrap results; THEN aggregate all of those and later run res_fun() to get VSPs and such 
-    # Should then, using some kind of cleverness, be able to call res.fun on the replicate() data
-}
+##     # Yeah, the above code is not right! Need to return a list, p + 1 coefficeients, or similarly a p+1 matrix of bootstrap results; THEN aggregate all of those and later run res_fun() to get VSPs and such 
+##     # Should then, using some kind of cleverness, be able to call res.fun on the replicate() data
+## }
